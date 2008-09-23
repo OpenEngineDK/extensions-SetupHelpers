e5118297543b092d908bf4a173f260a28532631c
zerny <zerny@openengine.dk>
2008-09-23 16:39:09 +0000

    Added a simple setup helper.

---
+++ Utils/SimpleSetup.cpp
@@ -0,0 +1,298 @@
+// Simple setup of a graphics engine in OpenEngine.
+// -------------------------------------------------------------------
+// Copyright (C) 2007 OpenEngine.dk (See AUTHORS) 
+// 
+// This program is free software; It is covered by the GNU General 
+// Public License version 2 or any later version. 
+// See the GNU General Public License for more details (see LICENSE). 
+//--------------------------------------------------------------------
+
+#include <Utils/SimpleSetup.h>
+
+// Core stuff
+#include <Core/Engine.h>
+#include <Display/Camera.h>
+#include <Display/Frustum.h>
+#include <Display/InterpolatedViewingVolume.h>
+#include <Display/ViewingVolume.h>
+#include <Renderers/TextureLoader.h>
+#include <Resources/ResourceManager.h>
+#include <Scene/DirectionalLightNode.h>
+#include <Scene/SceneNode.h>
+
+// Acceleration extension
+#include <Renderers/AcceleratedRenderingView.h>
+#include <Scene/ASDotVisitor.h>
+
+// OpenGL extension
+#include <Renderers/OpenGL/Renderer.h>
+#include <Renderers/OpenGL/RenderingView.h>
+
+// SDL extension
+#include <Display/SDLFrame.h>
+#include <Devices/SDLInput.h>
+
+// Resources extensions
+#include <Resources/OBJResource.h>
+#include <Resources/TGAResource.h>
+#include <Resources/ColladaResource.h>
+
+// Logging
+#include <Logging/Logger.h>
+#include <Logging/StreamLogger.h>
+
+namespace OpenEngine {
+namespace Utils {
+
+using namespace Core;
+using namespace Devices;
+using namespace Display;
+using namespace Logging;
+using namespace Renderers::OpenGL;
+using namespace Renderers;
+using namespace Resources;
+using namespace Scene;
+
+class ExtRenderingView
+    : public RenderingView
+    , public AcceleratedRenderingView {
+public:
+    ExtRenderingView(Viewport& viewport)
+        : IRenderingView(viewport)
+        , RenderingView(viewport)
+        , AcceleratedRenderingView(viewport) {}
+};
+
+class TextureLoadOnInit
+    : public IListener<RenderingEventArg> {
+    TextureLoader& tl;
+public:
+    TextureLoadOnInit(TextureLoader& tl) : tl(tl) { }
+    void Handle(RenderingEventArg arg) {
+        if (arg.renderer.GetSceneRoot() != NULL)
+            tl.Load(*arg.renderer.GetSceneRoot());
+    }
+};
+
+class QuitHandler : public IListener<KeyboardEventArg> {
+    IEngine& engine;
+public:
+    QuitHandler(IEngine& engine) : engine(engine) {}
+    void Handle(KeyboardEventArg arg) {
+        if (arg.sym == KEY_ESCAPE) engine.Stop();
+    }
+};
+
+/**
+ * Create the simple setup helper.
+ * This will create all of the engine components. After this you may
+ * setup any additional project related components and then invoke
+ * @code setup.GetEngine().Start() @endcode to start the engine. After
+ * invoking the engine start the ordinary
+ * initialize/process/deinitialize phases will proceed as usual.
+ *
+ * @param title Project title
+ */
+SimpleSetup::SimpleSetup(std::string title)
+    : title(title)
+    , engine(new Engine())
+    , frame(new SDLFrame(800,600,32))
+    , renderer(new Renderer())
+    , input(new SDLInput())
+    , scene(new SceneNode())
+    , camera(new Camera(*(new InterpolatedViewingVolume(*(new ViewingVolume())))))
+    , frustum(new Frustum(*camera))
+    , viewport(new Viewport(*frame))
+    , renderingview(new ExtRenderingView(*viewport))
+    , textureloader(new TextureLoader(*renderer))
+{
+    // create a logger to std out
+    Logger::AddLogger(new StreamLogger(&std::cout));
+    // configure modules needing process time
+    engine->InitializeEvent().Attach(*frame);
+    engine->InitializeEvent().Attach(*renderer);
+    engine->InitializeEvent().Attach(*input);
+    engine->ProcessEvent().Attach(*frame);
+    engine->ProcessEvent().Attach(*renderer);
+    engine->ProcessEvent().Attach(*input);
+    engine->DeinitializeEvent().Attach(*frame);
+    engine->DeinitializeEvent().Attach(*renderer);
+    engine->DeinitializeEvent().Attach(*input);
+    // add plug-ins
+    ResourceManager<IModelResource>::AddPlugin(new OBJPlugin());
+    ResourceManager<ITextureResource>::AddPlugin(new TGAPlugin());
+    // populate the default scene
+    scene->AddNode(new DirectionalLightNode());
+    // setup the rendering system
+    renderer->ProcessEvent().Attach(*renderingview);
+    renderer->SetSceneRoot(scene);
+    viewport->SetViewingVolume(frustum);
+    renderer->InitializeEvent().Attach(*(new TextureLoadOnInit(*textureloader)));
+    // bind default keys
+    input->KeyEvent().Attach(*(new QuitHandler(*engine)));
+}
+
+/**
+ * Get the engine.
+ * The engine can not be replaced.
+ * @see IEngine
+ */
+IEngine& SimpleSetup::GetEngine() const {
+    return *engine;
+}
+
+/**
+ * Get the frame.
+ * The default frame is will have the dimensions 800x600 at color depth 32.
+ * The frame can not be replaced.
+ * In order to change the frame use the set-methods defined in IFrame.
+ * @see IFrame
+ */
+IFrame& SimpleSetup::GetFrame() const {
+    return *frame;
+}
+
+/**
+ * Get the renderer.
+ * The renderer will automatically be supplied with a rendering
+ * view that uses the full frame as its viewport.
+ * Additionally it will always render the current scene supplied
+ * by SetScene().
+ * The renderer itself is not replaceable.
+ */
+IRenderer& SimpleSetup::GetRenderer() const {
+    return *renderer;
+}
+
+/**
+ * Get the mouse.
+ * The mouse structure is not replaceable.
+ */
+IMouse& SimpleSetup::GetMouse() const {
+    return *input;
+}
+
+/**
+ * Get the keyboard.
+ * The keyboard structure is not replaceable.
+ */
+IKeyboard& SimpleSetup::GetKeyboard() const {
+    return *input;
+}
+
+/**
+ * Get the joystick.
+ * The joystick structure is not replaceable.
+ */
+IJoystick& SimpleSetup::GetJoystick() const {
+    return *input;
+}
+
+/**
+ * Get the current scene.
+ * The default scene consists of a SceneNode with a single
+ * DirectionalLightNode beneath it.
+ * If you wish to get rid of the light node simply delete the
+ * scene and attache a new one that suits you purpose with
+ * SetScene().
+ * The current scene during renderer initialization will be
+ * searched for textures to load.
+ */
+ISceneNode* SimpleSetup::GetScene() const {
+    return scene;
+}
+
+/**
+ * Set the current scene.
+ * This will automatically update the renderer to use the new
+ * scene.
+ * Ownership of the scene remains with the caller but it is
+ * assumed to be non-null as long as it is active.
+ * If replacing a scene it is the callers responsibility to clean
+ * up the old scene if needed by using GetScene().
+ * When setting a new scene it will automatically be searched for
+ * textures to load.
+ * @param scene New active scene.
+ */
+void SimpleSetup::SetScene(ISceneNode& scene) {
+    this->scene = &scene;
+    renderer->SetSceneRoot(this->scene);
+    textureloader->Load(scene);
+}
+
+/**
+ * Get the current camera.
+ * The default camera is placed in origin (0,0,0) following the
+ * z-axis in the negative direction (0,0,-1).
+ */
+Camera* SimpleSetup::GetCamera() const {
+    return camera;
+}
+
+/**
+ * Set the current camera.
+ * Ownership of the camera remains with the caller.
+ */
+void SimpleSetup::SetCamera(Camera& volume) {
+    camera = &volume;
+    delete frustum;
+    frustum = new Frustum(*camera);
+    viewport->SetViewingVolume(frustum);
+}
+/**
+ * Set a camera by viewing volume.
+ * The non-camera class is used to create a *new* camera that wraps
+ * the viewing volume. The new camera is created on the heap and the
+ * caller is responsible for any needed clean-up of this structure.
+ *
+ * @param volume Volume to wrap in a *new* camera.
+ */
+void SimpleSetup::SetCamera(IViewingVolume& volume) {
+    camera = new Camera(volume);
+    viewport->SetViewingVolume(camera);
+}
+
+/**
+ * Add a data directory to the file search path.
+ * This path will be searched when loading file resources.
+ * By default we load all the resource plug-ins we can.
+ *
+ * @param dir Directory with file resources
+ */
+void SimpleSetup::AddDataDirectory(string dir) {
+    DirectoryManager::AppendPath(dir);
+}
+
+/**
+ * Enable various run-time debugging features.
+ * This includes
+ * - visualization of the frustum,
+ * - export the scene to a dot-graph file (scene.dot)
+ * - add FPS to the HUD (not implemented yet)
+ *
+ * @todo: add cairo fps to the hud
+ */
+void SimpleSetup::EnableDebugging() {
+    // Visualization of the frustum
+    frustum->VisualizeClipping(true);
+    scene->AddNode(frustum->GetFrustumNode());
+
+    // @todo: this should be on initialize
+    // Output a dot-graph of the scene
+    ofstream dotfile("scene.dot", ofstream::out);
+    if (!dotfile.good()) {
+        logger.error << "Can not open 'scene.dot' for output"
+                     << logger.end;
+    } else {
+        ASDotVisitor dot;
+        dot.Write(*scene, &dotfile);
+        logger.info << "Saved physics graph to 'scene.dot'"
+                    << logger.end
+                    << "To create a SVG image run: "
+                    << "dot -Tsvg scene.dot > scene.svg"
+                    << logger.end;
+    }
+}
+
+} // NS Utils
+} // NS OpenEngine
---
+++ Utils/SimpleSetup.h
@@ -0,0 +1,146 @@
+// Simple setup of a graphics engine in OpenEngine.
+// -------------------------------------------------------------------
+// Copyright (C) 2007 OpenEngine.dk (See AUTHORS) 
+// 
+// This program is free software; It is covered by the GNU General 
+// Public License version 2 or any later version. 
+// See the GNU General Public License for more details (see LICENSE). 
+//--------------------------------------------------------------------
+
+#ifndef _OE_SIMPLE_SETUP_H_
+#define _OE_SIMPLE_SETUP_H_
+
+#include <Meta/Config.h>
+
+// include the main interfaces
+#include <Core/IEngine.h>
+#include <Devices/IJoystick.h>
+#include <Devices/IKeyboard.h>
+#include <Devices/IMouse.h>
+#include <Display/Camera.h>
+#include <Display/IFrame.h>
+#include <Renderers/IRenderer.h>
+#include <Scene/ISceneNode.h>
+#include <Resources/IModelResource.h>
+#include <Resources/ITextureResource.h>
+
+// include all the classes that depend on serialization
+#include <Resources/TGAResource.h>
+// @todo: what about all the scene nodes?
+
+// other std c++ includes
+#include <string>
+
+// forward declarations
+namespace OpenEngine {
+    namespace Core {
+        class Engine;
+    }
+    namespace Display {
+        class Viewport;
+        class IViewingVolume;
+        class Frustum;
+        class SDLFrame;
+    }
+    namespace Devices {
+        class SDLInput;
+    }
+    namespace Scene {
+        class SceneNode;
+    }
+    namespace Renderers {
+        class TextureLoader;
+        namespace OpenGL {
+            class Renderer;
+            class RenderingView;
+        }
+    }
+}
+
+namespace OpenEngine {
+namespace Utils {
+
+/**
+ * The purpose of simple setup is to provide a fairly basic setup of
+ * an OpenEngine graphics engine.
+ * The interface should be as stable as possible so that it can act
+ * as a barrier between core and extension changes reducing the amount
+ * of code breakage.
+ * It is not meant to solve all setups. More advanced products may
+ * very well need to revert to setting up the entire engine from
+ * scratch or they may be based on a more advanced setup wrapper.
+ * 
+ * Using SimpleSetup as a base will allow a project to simply link to
+ * this extension alone. SimpleSetup will link to all other required
+ * components. What about namespace stuff?
+ *
+ * This example interface is based on what appears in all the current
+ * "projects" built on top of OpenEngine.
+ *
+ * @code
+ * // How to start an empty screen with a 800x600 frame.
+ * #include <Utils/SimpleSetup.h>
+ * using OpenEngine::Utils::SimpleSetup;
+ * int main() {
+ *   SimpleSetup setup;
+ *   setup.GetEngine().Start();
+ *   return 0;
+ * }
+ * @endcode
+ */
+class SimpleSetup {
+public:
+
+    SimpleSetup(std::string title);
+
+    Core::IEngine& GetEngine() const;
+    Display::IFrame& GetFrame() const;
+    Renderers::IRenderer& GetRenderer() const;
+
+    Devices::IMouse&    GetMouse() const;
+    Devices::IKeyboard& GetKeyboard() const;
+    Devices::IJoystick& GetJoystick() const;
+
+    Scene::ISceneNode* GetScene() const;
+    void SetScene(Scene::ISceneNode& scene);
+
+    Display::Camera* GetCamera() const;
+    void SetCamera(Display::Camera& volume);
+    void SetCamera(Display::IViewingVolume& volume);
+
+    void AddDataDirectory(std::string dir);
+
+    void EnableDebugging();
+
+    // What about:
+    // - HUD.
+    // - Sound.
+    // - Lighting.
+    // - Physics.
+    // - Input mappings.
+    // - State machines.
+    // Shall we let them wait? Not part of a simple setup?
+
+    // Should we consider a more general Setup-System that consists of
+    // different setup objects? What would such a system require and
+    // why would it not suffer from the complexity of the current
+    // structure?
+
+private:
+    std::string title;
+    Core::Engine* engine;
+    Display::SDLFrame* frame;
+    Renderers::OpenGL::Renderer* renderer;
+    Devices::SDLInput* input;
+    Scene::ISceneNode* scene;
+    Display::Camera* camera;
+    Display::Frustum* frustum;
+    Display::Viewport* viewport;
+    Renderers::OpenGL::RenderingView* renderingview;
+    Renderers::TextureLoader* textureloader;
+};
+
+} // NS Utils
+} // NS OpenEngine
+
+#endif // _OE_SIMPLE_SETUP_H_
