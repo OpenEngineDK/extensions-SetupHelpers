d272327fc428cfc071003f7adedf2867bda2924d
salomonhippie <salomonhippie@gmail.com>
2010-11-04 15:23:25 +0000

    new canvas stuff.

--- Utils/SimpleSetup.cpp
+++ Utils/SimpleSetup.cpp
@@ -35,7 +35,8 @@
 // SDL extension
 #include <Display/SDLEnvironment.h>
 
-#include <Display/OpenGL/RenderCanvas.h>
+#include <Display/RenderCanvas.h>
+#include <Display/OpenGL/TextureCopy.h>
 
 // Resources extensions
 #include <Resources/OBJResource.h>
@@ -150,7 +151,7 @@ public:
     // setup a default viewport and camera
     camera  = new Camera(*(new PerspectiveViewingVolume()));
     frustum = new Frustum(*camera);
-    canvas = new RenderCanvas();
+    canvas = new RenderCanvas(new TextureCopy());
     canvas->SetViewingVolume(frustum);
     
     //viewport->SetViewingVolume(frustum);
